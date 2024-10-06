650fe255a31e6fbb20638847946055231c1f0702
lisagjh <lisagjhoorn@outlook.com>
2024-10-06 17:12:20 +0200

    content collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,28 @@
-// 1. Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 
-// multiple collections: https://docs.astro.build/en/guides/content-collections/#defining-multiple-collections
-// 2. Define a `type` and `schema` for each collection
-const blogCollection = defineCollection({
-  type: "content", // v2.5.0 and later
+const postsCollection = defineCollection({
+    schema: z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: z.object({
+        url: z.string(),
+        alt: z.string()
+      }),
+      tags: z.array(z.string())
+    })
+ });
+
+const webCollection = defineCollection({
   schema: z.object({
     title: z.string(),
-    tags: z.array(z.string()),
-    image: z.string().optional(),
-  }),
+    date: z.date(),
+    description: z.string(),
+    tags: z.array(z.string())
+  })
 });
 
 export const collections = {
-  blog: blogCollection,
+  posts: postsCollection,
+  weloveweb: webCollection
 };
