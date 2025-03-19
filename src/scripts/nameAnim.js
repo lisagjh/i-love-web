7b64f1dcbaf545815728cd6a44c0bdfde2440942
lisagjh <lisagjhoorn@outlook.com>
2025-03-19 13:25:21 +0100

    if3b

--- src/scripts/nameAnim.js
+++ src/scripts/nameAnim.js
@@ -1,47 +1,53 @@
 import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
 gsap.registerPlugin(ScrollTrigger);
 
-function getPathLength(selector) {
-  const path = document.querySelector(selector);
-  return path ? path.getTotalLength() : 0;
-}
+document.addEventListener("DOMContentLoaded", () => {
+  function getPathLength(selector) {
+    const path = document.querySelector(selector);
+    return path ? path.getTotalLength() : 0;
+  }
 
-const lLength = getPathLength(".l");
-const iLength = getPathLength(".i");
-const sLength = getPathLength(".s");
-const aLength = getPathLength(".a");
+  const lLength = getPathLength(".l");
+  const iLength = getPathLength(".i");
+  const sLength = getPathLength(".s");
+  const aLength = getPathLength(".a");
 
-gsap.set(".l, .i, .s, .a", {
-  opacity: 0,
-  strokeDashoffset: (i) => [lLength, iLength, sLength, aLength][i],
-  strokeDasharray: (i) => [lLength, iLength, sLength, aLength][i],
-  y: -30,
-});
+  gsap.set(".l, .i, .s, .a", {
+    opacity: 0,
+    strokeDashoffset: (i) => [lLength, iLength, sLength, aLength][i],
+    strokeDasharray: (i) => [lLength, iLength, sLength, aLength][i],
+    y: -30,
+  });
 
-const tl = gsap.timeline({ delay: 0.2 });
+  const tl = gsap.timeline({ delay: 0.2 });
 
-tl.to(".l, .i, .s, .a", {
-  opacity: 1,
-  strokeDashoffset: 0,
-  duration: 0.8,
-  ease: "power2.out",
-  stagger: 0.1,
-});
+  tl.to(".l, .i, .s, .a", {
+    opacity: 1,
+    transformOrigin: "center center",
+    strokeDashoffset: 0,
+    duration: 0.8,
+    ease: "power2.out",
+    stagger: 0.1,
+  });
 
-tl.to(
-  ".l",
-  {
-    y: 0,
-    duration: 1.5,
-    ease: "bounce.out",
-  },
-  "-=0.75"
-)
-  .to(".i", { y: 0, duration: 0.5, ease: "elastic.out(1,0.3)" }, "-=0.5")
-  .to(".s", { y: 0, duration: 0.5, ease: "elastic.out(1,0.3)" }, "-=0.5")
-  .to(".a", { y: 0, duration: 0.5, ease: "elastic.out(1,0.3)" }, "-=0.5");
+  tl.to(".l", { y: 0, duration: 1.5, ease: "bounce.out" }, "-=0.75")
+    .to(".i", { y: 0, duration: 0.5, ease: "power4.out" }, "-=0.5")
+    .to(".s", { y: 0, duration: 0.5, ease: "power4.out" }, "-=0.5")
+    .to(".a", { y: 0, duration: 0.5, ease: "power4.out" }, "-=0.5");
 
-gsap.to(".name", {
-  scrollTrigger: ".box", // start animation when ".box" enters the viewport
-  x: 500,
+  document.querySelectorAll(".l, .i, .s, .a").forEach((letter) => {
+    gsap.to(letter, {
+      scrollTrigger: {
+        trigger: ".section_full-width",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true,
+      },
+      yPercent: gsap.utils.random(-20, 30), // Each letter moves up randomly
+      rotate: gsap.utils.random(-10, 10),
+      ease: "power1.out",
+    });
+  });
 });
